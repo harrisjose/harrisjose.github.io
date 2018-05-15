b9ab4f6ceef1632186d52772256b702c1ae1dc7c
Harris Jose <harrisconnected@gmail.com>
2018-05-15 23:53:48 +0530

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-b424ce760c43069f0455.js","1ffb11bcf4ab1c699af81cc1a4432a95"],["/commons-1a128bda0853ec7731a6.js","aea669e1efd8da4860ae21c807192bcd"],["/commons-452e68938f0c208d018d.js","34535b8842f905c6d29b4bc867b828be"],["/commons-57b911000cc8d2318c15.js","a96820daa089368b8f9dd721ee14974c"],["/index.html","ea4ed29cb168272d359d6274da71a445"],["/offline-plugin-app-shell-fallback/index.html","c52fa8773f161c5a1a7dc23353bae15c"]];
+var precacheConfig = [["/app-b424ce760c43069f0455.js","1ffb11bcf4ab1c699af81cc1a4432a95"],["/commons-1a128bda0853ec7731a6.js","aea669e1efd8da4860ae21c807192bcd"],["/commons-452e68938f0c208d018d.js","34535b8842f905c6d29b4bc867b828be"],["/commons-57b911000cc8d2318c15.js","a96820daa089368b8f9dd721ee14974c"],["/index.html","062489dc3e4832117016701fe365d587"],["/offline-plugin-app-shell-fallback/index.html","cdd68e45cee102b0e896d6ac4cda03ea"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
