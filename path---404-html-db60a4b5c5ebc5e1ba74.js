85d35c34dec23d029d700d7398cf02acaf10626c
Harris Jose <harrisconnected@gmail.com>
2018-07-29 15:36:49 +0530

    Updates

--- path---404-html-db60a4b5c5ebc5e1ba74.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0xa2868bfb69fc],{352:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Harris Jose"}}},pathContext:{}}}});
-//# sourceMappingURL=path---404-html-db60a4b5c5ebc5e1ba74.js.map
--- path---about-db60a4b5c5ebc5e1ba74.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0xf927f8900006],{354:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Harris Jose"}}},pathContext:{}}}});
-//# sourceMappingURL=path---about-db60a4b5c5ebc5e1ba74.js.map
